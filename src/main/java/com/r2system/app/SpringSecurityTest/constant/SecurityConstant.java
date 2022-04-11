f90b1d9937e5af014bb624da5b18996637f74337
Simbak Ramirez Gonzalez <simbak.ramirez@gmail.com>
2022-04-11 16:39:28 -0600

    05-JPA CONNECTION V2

---
+++ src/main/java/com/r2system/app/SpringSecurityTest/constant/SecurityConstant.java
@@ -0,0 +1,9 @@
+package com.r2system.app.SpringSecurityTest.constant;
+
+public class SecurityConstant {
+
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final String AUTH_HEADER = "authorization";
+    public static final String AUTH_TOKEN_TYPE = "Bearer";
+    public static final String AUTH_TOKEN_PREFIX = AUTH_TOKEN_TYPE + " ";
+}
--- src/main/java/com/r2system/app/SpringSecurityTest/service/JpaUserDetailService.java
+++ src/main/java/com/r2system/app/SpringSecurityTest/service/JpaUserDetailService.java
@@ -1,8 +1,10 @@
 package com.r2system.app.SpringSecurityTest.service;
 
+
 import com.r2system.app.SpringSecurityTest.model.Roles;
 import com.r2system.app.SpringSecurityTest.model.Users;
 import com.r2system.app.SpringSecurityTest.repository.UserRepository;
+import com.r2system.app.SpringSecurityTest.util.SecurityAuthorities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,11 +14,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 @Service("jpaUserDetailService")
@@ -32,25 +33,23 @@ public class JpaUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 
-        Optional<Users> user =  _repository.findByUsername(username);
-
-        if(user == null){
-          log.error("Error en el Login: no existe el usuario '\" + username + \"' en el sistema!");
-          throw new UsernameNotFoundException("Username: \" + username + \" no existe en el sistema!");
-        }
-
+        Users user =  _repository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException(username));
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+       // user.getRoles().stream().forEach((roles -> new SimpleGrantedAuthority(roles.getName().toString())));
+        // Set<GrantedAuthority> authorities = Set.of(SecurityAuthorities.convertToAuthority(user.getRoles().stream().toArray().toString()));
 
-        for(Roles role : user.get().getRoles()){
-
+        for(Roles role : user.getRoles()){
             authorities.add(new SimpleGrantedAuthority(role.getName().toString()));
         }
 
-        if(authorities.isEmpty()) {
+        log.debug(authorities.toString());
+       if(authorities.isEmpty()) {
             log.error("Error en el Login: Usuario '" + username + "' no tiene roles asignados!");
             throw new UsernameNotFoundException("Error en el Login: usuario '" + username + "' no tiene roles asignados!");
         }
 
-        return new User(user.get().getUsername(),user.get().getPassword(), user.get().isEnabled(), true, true, true, authorities);
+        return new User(user.getUsername(),user.getPassword(), user.isEnabled(), true,
+                true, true, authorities);
+
     }
 }
