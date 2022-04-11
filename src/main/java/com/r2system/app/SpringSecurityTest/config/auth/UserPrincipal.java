5e6271b9b3f69e184d9a5e2ccce0fe01b48eecf8
Simbak Ramirez Gonzalez <simbak.ramirez@gmail.com>
2022-04-11 16:55:16 -0600

    06- JPA CONNETION V3 BUILDER USER

---
+++ src/main/java/com/r2system/app/SpringSecurityTest/config/auth/UserPrincipal.java
@@ -0,0 +1,61 @@
+package com.r2system.app.SpringSecurityTest.config.auth;
+
+import com.r2system.app.SpringSecurityTest.model.Users;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserPrincipal  implements UserDetails {
+
+    transient private Users user; //user for only login operation, don't use in JWT.
+    private List<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
+}
--- src/main/java/com/r2system/app/SpringSecurityTest/service/JpaUserDetailService.java
+++ src/main/java/com/r2system/app/SpringSecurityTest/service/JpaUserDetailService.java
@@ -1,15 +1,14 @@
 package com.r2system.app.SpringSecurityTest.service;
 
 
+import com.r2system.app.SpringSecurityTest.config.auth.UserPrincipal;
 import com.r2system.app.SpringSecurityTest.model.Roles;
 import com.r2system.app.SpringSecurityTest.model.Users;
 import com.r2system.app.SpringSecurityTest.repository.UserRepository;
-import com.r2system.app.SpringSecurityTest.util.SecurityAuthorities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,7 +16,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @Service("jpaUserDetailService")
@@ -35,8 +33,6 @@ public class JpaUserDetailService implements UserDetailsService {
 
         Users user =  _repository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException(username));
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-       // user.getRoles().stream().forEach((roles -> new SimpleGrantedAuthority(roles.getName().toString())));
-        // Set<GrantedAuthority> authorities = Set.of(SecurityAuthorities.convertToAuthority(user.getRoles().stream().toArray().toString()));
 
         for(Roles role : user.getRoles()){
             authorities.add(new SimpleGrantedAuthority(role.getName().toString()));
@@ -48,8 +44,10 @@ public class JpaUserDetailService implements UserDetailsService {
             throw new UsernameNotFoundException("Error en el Login: usuario '" + username + "' no tiene roles asignados!");
         }
 
-        return new User(user.getUsername(),user.getPassword(), user.isEnabled(), true,
-                true, true, authorities);
+        return UserPrincipal.builder()
+                .user(user)
+                .authorities(authorities)
+                .build();
 
     }
 }
